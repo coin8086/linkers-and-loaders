156e22331b7acd1aa75b0ba110b7cffbc0636606
Robert Zhang <louirobert@gmail.com>
2022-07-26 18:20:17 +0800

    Add symbol-versioning

---
+++ symbol-versioning/README.md
@@ -0,0 +1,1 @@
+Refer to https://man7.org/conf/lca2006/shared_libraries/slide19a.html
---
+++ symbol-versioning/sv_lib_v1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void xyz(void)
+{
+  printf("v1 xyz\n");
+}
+
+void abc(void)
+{
+  printf("v1 abc\n");
+}
